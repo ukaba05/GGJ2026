5f7f09af66e2160919350b010055b079c79eec93
ukaba05 <ukaba05@gmail.com>
2026-01-31 02:02:50 +0800

    新增玩家移動

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed;
+
+    [SerializeField]
+    Rigidbody2D _rigidbody;
+
+    void Update() {
+        var direction = MapInputToDirection();
+        _rigidbody.velocity = direction * _speed;
+    }
+
+    static Vector2 MapInputToDirection() {
+        var direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+
+        return direction;
+    }
+}
